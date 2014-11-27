814eb5e62933e7f89e3aaa4db51b9fa6cd0b523a
Wesley Smits <wsmits@intermesh.nl>
2014-11-27 13:54:24 +0100

    Added css

--- app/modules/projects/js/module.js
+++ app/modules/projects/js/module.js
@@ -1,23 +1,27 @@
 'use strict';
 
 angular.module('GO').
-				//Register the app
-				config(['modulesProvider',function(modulesProvider) {
-						modulesProvider.addModule('projects', 'Projects','fa-tasks');
-					}]).
+	//Register the app
+	config(['modulesProvider', function (modulesProvider) {
+			modulesProvider.addModule('projects', 'Projects', 'fa-tasks');
+		}]).
+	config(['$stateProvider', function ($stateProvider) {
 
-				config(['$stateProvider', function($stateProvider) {
-
-						// Now set up the states
-						$stateProvider
-							.state('projects', {
-								url: "/Projects",
-								templateUrl: 'modules/projects/partials/main.html',
-								controller: 'ProjectController'
-							})
-							.state('projects.edit', {
-								url: "/Projects/edit/{projectId:[0-9]*}",
-								controller: 'ProjectEditController',
-								template: '<div ng-init="openModal()"></div>'
-							});
-					}]);
+			// Now set up the states
+			$stateProvider
+				.state('projects', {
+					url: "/projects",
+					templateUrl: 'modules/projects/partials/main.html',
+					controller: 'ProjectController'
+				})
+				.state('projects.edit', {
+					url: "/edit/{projectId:[0-9]*}",
+					templateUrl: 'modules/projects/partials/project-edit.html',
+					controller: 'ProjectEditController'
+				})
+				.state('projects.project.detail', {
+					url: "/detail/{projectId:[0-9]*}",
+					templateUrl: 'modules/projects/partials/project-detail.html',
+					controller: 'ProjectDetailController'
+				});
+		}]);
